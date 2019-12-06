0ee2557fc0e043c7998faef1dc17b0aba38c2ff4
Colin Fraser <colin.r.fraser@gmail.com>
2019-12-05 17:41:37 -0800

    add test files to repo so that tests are portable

--- tests/testthat/test-responses.R
+++ tests/testthat/test-responses.R
@@ -1,11 +1,8 @@
 test_that("Parsing responses works correctly", {
-  expect_equal(data_response$fields, c(
-    "ad_creation_time", "ad_creative_body", "ad_creative_link_caption",
-    "ad_creative_link_description", "ad_creative_link_title", "ad_delivery_start_time",
-    "ad_delivery_stop_time", "ad_snapshot_url", "currency", "demographic_distribution",
-    "funding_entity", "impressions", "page_id", "page_name", "publisher_platforms",
-    "region_distribution", "spend"
-  ))
+  expect_equal(data_response$fields, c("ad_creation_time", "ad_creative_body", "ad_creative_link_caption",
+                                       "ad_creative_link_description", "ad_creative_link_title", "ad_delivery_start_time",
+                                       "ad_delivery_stop_time", "currency", "funding_entity", "page_id",
+                                       "page_name", "spend", "impressions", "ad_snapshot_url"))
 })
 
 test_that("Converting to ad table works", {
