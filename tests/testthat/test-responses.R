e799b81b402d5f6ed5a79753b49852ca1af908c8
Colin Fraser <colin.r.fraser@gmail.com>
2019-12-06 20:18:13 -0800

    try loading files in test file

--- tests/testthat/test-responses.R
+++ tests/testthat/test-responses.R
@@ -1,4 +1,6 @@
 test_that("Parsing responses works correctly", {
+  http_response <- readRDS("example_response.RDS")
+  data_response <- adlib_data_response(http_response)
   expect_equal(data_response$fields, c(
     "ad_creation_time", "ad_creative_body", "ad_creative_link_caption",
     "ad_creative_link_description", "ad_creative_link_title", "ad_delivery_start_time",
@@ -8,6 +10,8 @@ test_that("Parsing responses works correctly", {
 })
 
 test_that("Converting to ad table works", {
+  http_response <- readRDS("example_response.RDS")
+  data_response <- adlib_data_response(http_response)
   ad_table <- ad_table(data_response)
   example_adtable <- readRDS("sample_adtable.rds")
   expect_equal(ad_table, example_adtable)
