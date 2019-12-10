7c67a53d92bf1b3e027953cc327bde5536cfb26d
Colin Fraser <colin.r.fraser@gmail.com>
2019-12-10 15:26:29 -0800

    resolves #35

--- R/adlib.R
+++ R/adlib.R
@@ -195,11 +195,12 @@ adlib_fields <- function(fields = FIELDS) {
 #' @param params a list of parameters.
 #' @param token an access_token.
 #'
-#' @description The `params` argument should be a list that is built with
-#' adlib_build_query().
+#' @description This function sends a request to the Ad Librar API.
+#' The `params` argument should be a list that is built with adlib_build_query().
 #' The `token` argument can be a short-term token pasted from
 #' https://developers.facebook.com/tools/explorer/,
-#' or a long-term token stored in your password store.
+#' or a long-term token stored in your password store. By default it will look
+#' for a token in your password store.
 #'
 #' @return an http response
 #' @export
