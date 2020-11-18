2a8f5919fe8807a9b51bd1af89cb041697060158
Colin Fraser <colin.r.fraser@gmail.com>
2020-11-18 15:07:35 -0800

    docs

--- tests/testthat/test-adlib.R
+++ tests/testthat/test-adlib.R
@@ -11,7 +11,10 @@ test_that("build_query returns a list as expected", {
     adlib_build_query("US", search_terms = "Facebook"),
     list(
       ad_active_status = "ACTIVE", ad_reached_countries = "['US']",
-      ad_type = "POLITICAL_AND_ISSUE_ADS", bylines = NULL, impression_condition = "HAS_IMPRESSIONS_LIFETIME",
+      ad_delivery_date_max = NULL,
+      ad_delivery_date_min = NULL,
+      ad_type = "POLITICAL_AND_ISSUE_ADS", bylines = NULL,
+      delivery_by_region = NULL,
       potential_reach_max = integer(0), potential_reach_min = integer(0), publisher_platform = "['FACEBOOK']", search_page_ids = NULL,
       search_terms = "Facebook", fields = "ad_snapshot_url,ad_creation_time,ad_creative_body,ad_creative_link_caption,ad_creative_link_description,ad_creative_link_title,ad_delivery_start_time,ad_delivery_stop_time,currency,funding_entity,page_id,page_name,spend,impressions,potential_reach",
       limit = 1000
