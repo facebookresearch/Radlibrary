f09d82e67772b1959a0d920bf36205ea87f5acfa
Colin Fraser <colinfraser@fb.com>
2022-03-28 13:23:25 -0700

    add skip_on_ci to tests that call the API

--- tests/testthat/test-requests.R
+++ tests/testthat/test-requests.R
@@ -12,6 +12,7 @@ q <- adlib_build_query(
 
 test_that("graph_get works", {
   skip_on_cran()
+  skip_on_ci()
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- graph_get("ads_archive", q, token)
   expect_equal(httr::status_code(resp), 200)
@@ -21,6 +22,7 @@ test_that("graph_get works", {
 
 test_that("adlib_get works", {
   skip_on_cran()
+  skip_on_ci()
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- adlib_get(q, token = token)
   expect_s3_class(resp, "adlib_data_response")
@@ -30,6 +32,7 @@ test_that("adlib_get works", {
 
 test_that("access token censoring in tibbles works", {
   skip_on_cran()
+  skip_on_ci()
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- adlib_get(q, token = token)
   resp_table <- tibble::as_tibble(resp, censor_access_token = FALSE)
@@ -41,6 +44,7 @@ test_that("access token censoring in tibbles works", {
 
 test_that("adlib_get_paginated works", {
   skip_on_cran()
+  skip_on_ci()
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- adlib_get_paginated(q, token = token, max_gets = 2)
   resp_table <- tibble::as_tibble(resp, censor_access_token = FALSE)
@@ -51,6 +55,7 @@ test_that("adlib_get_paginated works", {
 
 test_that("adlib_get works with no spend or impressions", {
   skip_on_cran()
+  skip_on_ci()
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   q <- adlib_build_query(
     ad_reached_countries = "US",
