c59d412003933eed0e59e6cdf470dc2474b4aa0d
Colin Fraser <colinfraser@fb.com>
2023-01-06 18:46:37 -0800

    remove tests that don't censor access token

--- tests/testthat/test-requests.R
+++ tests/testthat/test-requests.R
@@ -27,7 +27,7 @@ test_that("adlib_get works", {
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- adlib_get(q, token = token)
   expect_s3_class(resp, "adlib_data_response")
-  resp_table <- tibble::as_tibble(resp, censor_access_token = FALSE)
+  resp_table <- tibble::as_tibble(resp, censor_access_token = TRUE)
   expect_true(tibble::is_tibble(resp_table))
 })
 
@@ -36,8 +36,6 @@ test_that("access token censoring in tibbles works", {
   skip_if_not(token_exists_in_env())
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- adlib_get(q, token = token)
-  resp_table <- tibble::as_tibble(resp, censor_access_token = FALSE)
-  expect_true(any(detect_access_token(resp_table$ad_snapshot_url)))
   resp_table <- tibble::as_tibble(resp, censor_access_token = TRUE)
   expect_false(any(detect_access_token(resp_table$ad_snapshot_url)))
   expect_warning(tibble::as_tibble(resp))
@@ -48,10 +46,9 @@ test_that("adlib_get_paginated works", {
   skip_if_not(token_exists_in_env())
   token <- Sys.getenv("FB_GRAPH_API_TOKEN")
   resp <- adlib_get_paginated(q, token = token, max_gets = 2)
-  resp_table <- tibble::as_tibble(resp, censor_access_token = FALSE)
+  resp_table <- tibble::as_tibble(resp, censor_access_token = TRUE)
   expect_true(tibble::is_tibble(resp_table))
   expect_false(any(detect_access_token(tibble::as_tibble(resp, censor_access_token = TRUE)$ad_snapshot_url)))
-  expect_true(any(detect_access_token(tibble::as_tibble(resp, censor_access_token = FALSE)$ad_snapshot_url)))
 })
 
 test_that("adlib_get works with no spend or impressions", {
